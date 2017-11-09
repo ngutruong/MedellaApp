edf9cfaa35d2bc8c80bd120d1adec1bd126bb359
ngutruong <31942698+ngutruong@users.noreply.github.com>
2017-11-09 15:50:10 -0500

    Version 0.8.4.5 Drawer layout for Profile page has been added

--- app/src/main/java/ca/mohawk/truongnguyen/appmedella/ProfileActivity.java
+++ app/src/main/java/ca/mohawk/truongnguyen/appmedella/ProfileActivity.java
@@ -1,12 +1,19 @@
 package ca.mohawk.truongnguyen.appmedella;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -16,14 +23,20 @@ import android.widget.TextView;
 
 import java.util.regex.Pattern;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
         CheckBox feetAndInches = (CheckBox)findViewById(R.id.chkFeetInches);
         final EditText editHeight = (EditText)findViewById(R.id.txtHeight);
@@ -66,6 +79,9 @@ public class ProfileActivity extends AppCompatActivity {
                 }
             }
         });
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_profile);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     public static final Pattern EMAIL_PATTERN = Pattern.compile(
@@ -196,5 +212,67 @@ public class ProfileActivity extends AppCompatActivity {
                     .setMessage("Thank you.")
                     .show();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_profile, menu); //Cancel out 3-dot menu in Health Activity page
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        //DRAWER NAVIGATION IN HOME PAGE
+        if (id == R.id.nav_amHome) {
+            Intent iHome = new Intent(this, HomeActivity.class);
+            startActivity(iHome);
+        } else if (id == R.id.nav_amActivity) {
+            Intent iHealth = new Intent(this, HealthActivity.class);
+            startActivity(iHealth);
+        } else if (id == R.id.nav_amList) {
+            //ListActivity.class not available yet
+        } else if (id == R.id.nav_amResults) {
+            Intent iResults = new Intent(this, ResultsActivity.class);
+            startActivity(iResults);
+        } else if (id == R.id.nav_amTrash) {
+            //TrashActivity.class not available yet
+        } else if (id == R.id.nav_amLogout) {
+            //Logout is not available at this moment
+            //Logout must not appear while registering account
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
